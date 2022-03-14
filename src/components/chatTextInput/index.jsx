b6141c04afad9f175d914ed13a8311a00921ed9c
Raúl Lara <ralpwd@gmail.com>
2022-03-14 01:14:25 -0500

    feat/chat funcional con tecla enter

--- src/components/chatTextInput/index.jsx
+++ src/components/chatTextInput/index.jsx
@@ -4,9 +4,10 @@ import send from "../../assets/img/send.png"
 import './index.scss'
 
 const ChatInput = ( {name, id, placeholder, handleChange} ) => {
+
   return (
     <div className='chatInput'>
-        <input name={name} id={id} placeholder={placeholder} />
+        <input name={name} id={id} placeholder={placeholder} onKeyDown={handleChange} />
         <SendButton image={send} alt='Enviar' sendMessage={handleChange} />
     </div>
   )
