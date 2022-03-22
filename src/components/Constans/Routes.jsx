80711e51e33f7002b17fd1a4f1f1a9c796df63d6
mellito <dayanalexismanrique@hotmail.com>
2022-03-22 17:15:30 -0500

    last change

--- src/components/Constans/Routes.jsx
+++ src/components/Constans/Routes.jsx
@@ -1,7 +1,5 @@
 export const HOME_ROUTE = '/';
-
 export const REGISTER_ROUTE = '/register';
 export const EDIT_ROUTE = '/edition';
-export const CHANGE_PASSWORD_ROUTE = '/changepassword';
 export const CHAT_ROUTE = '/chat';
 export const GAME_ROUTE = '/game';
--- src/components/MainRouter/index.jsx
+++ src/components/MainRouter/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
-  HOME_ROUTE, REGISTER_ROUTE, EDIT_ROUTE, CHANGE_PASSWORD_ROUTE, CHAT_ROUTE, GAME_ROUTE,
+  HOME_ROUTE, REGISTER_ROUTE, EDIT_ROUTE, CHAT_ROUTE, GAME_ROUTE,
 } from '../Constans/Routes';
 import Register from '../../page/Register';
 import Home from '../../page/Home';
 import Edition from '../../page/Edition';
-import ChangePassword from '../../page/ChangePassword';
 import ChatBox from '../ChatBox';
 import Game from '../../page/Game';
 
@@ -17,7 +16,6 @@ function Mainrouter() {
         <Route path={HOME_ROUTE} element={<Home />} />
         <Route path={REGISTER_ROUTE} element={<Register />} />
         <Route path={EDIT_ROUTE} element={<Edition />} />
-        <Route path={CHANGE_PASSWORD_ROUTE} element={<ChangePassword />} />
         <Route path={CHAT_ROUTE} element={<ChatBox />} />
         <Route path={GAME_ROUTE} element={<Game />} />
       </Routes>
