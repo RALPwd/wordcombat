61990c2db5f8c471e6b94ee0ac169c1806cd8399
Raúl Lara <ralpwd@gmail.com>
2022-03-22 14:31:20 -0500

    Añadido chat a las rutas

--- src/components/Constans/Routes.jsx
+++ src/components/Constans/Routes.jsx
@@ -3,3 +3,4 @@ export const HOME_ROUTE = '/';
 export const REGISTER_ROUTE = '/register';
 export const EDIT_ROUTE = '/edition';
 export const CHANGE_PASSWORD_ROUTE = '/changepassword';
+export const CHAT_ROUTE = '/chat';
