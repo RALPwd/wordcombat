1fc72fa0bc21805b088af825e823196126e604d0
CristianCortes <corys90@hotmail.com>
2022-03-22 13:10:22 -0500

    Creaciòn de Pages Edicion y Cambio de password

--- src/components/MainRouter/index.jsx
+++ src/components/MainRouter/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HOME_ROUTE, REGISTER_ROUTE } from '../Constans/Routes';
+import {
+  HOME_ROUTE, REGISTER_ROUTE, EDIT_ROUTE, CHANGE_PASSWORD_ROUTE,
+} from '../Constans/Routes';
 import Register from '../../page/Register';
 import Home from '../../page/Home';
+import Edition from '../../page/Edition';
+import ChangePassword from '../../page/ChangePassword';
 
 function Mainrouter() {
   return (
@@ -10,7 +14,8 @@ function Mainrouter() {
       <Routes>
         <Route path={HOME_ROUTE} element={<Home />} />
         <Route path={REGISTER_ROUTE} element={<Register />} />
-        <Route path="invoices" element="" />
+        <Route path={EDIT_ROUTE} element={<Edition />} />
+        <Route path={CHANGE_PASSWORD_ROUTE} element={<ChangePassword />} />
       </Routes>
     </BrowserRouter>
   );
